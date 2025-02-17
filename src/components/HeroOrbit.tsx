a04801067667c91ff2795cb3a52a56db136b7d74
Mark Jerly Bundalian <mbundalian@fullscale.io>
2025-02-17 15:34:16 +0800

    Enhance HeroOrbit component with orbiting and spinning animations; update Stars component to utilize new props for dynamic animation durations.

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,29 +1,57 @@
 import StarIcon from "@/assets/icons/star.svg";
 import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 type heroOrbitProps = {
   size: number;
   rotation: number;
-}
+  shouldOrbit?: boolean;
+  orbitDuration?: string;
+  shouldSpin?: boolean;
+  spinDuration?: string;
+};
 
 const HeroOrbit = ({
   children,
   size,
   rotation,
+  shouldOrbit = false,
+  orbitDuration = "10s",
+  shouldSpin = false,
+  spinDuration = "10s",
 }: PropsWithChildren<heroOrbitProps>) => {
   return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20">
       <div
-        className="flex items-start justify-start"
+        className={twMerge(shouldOrbit === true && "animate-spin")}
         style={{
-          width: `${size}px`,
-          height: `${size}px`,
-          transform: `rotate(${rotation}deg)`,
+          animationDuration: orbitDuration,
         }}
       >
-        <div className="inline-flex" style={{
-          transform: `rotate(${rotation * -1}deg)`,
-        }}>{children}</div>
+        <div
+          className="flex items-start justify-start"
+          style={{
+            width: `${size}px`,
+            height: `${size}px`,
+            transform: `rotate(${rotation}deg)`,
+          }}
+        >
+          <div
+            className={twMerge(shouldSpin === true && "animate-spin")}
+            style={{
+              animationDuration: spinDuration,
+            }}
+          >
+            <div
+              className="inline-flex"
+              style={{
+                transform: `rotate(${rotation * -1}deg)`,
+              }}
+            >
+              {children}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
